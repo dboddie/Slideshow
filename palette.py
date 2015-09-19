679879e4f3d8f68fafc78419b87bec992536d816
David Boddie <david@boddie.org.uk>
2015-09-19 17:17:31 +0200

    Added license headers.

--- palette.py
+++ palette.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+"""
+Copyright (C) 2015 David Boddie <david@boddie.org.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 colours = {"black": (0, 0, 0),  "red":  (1, 0, 0), "green":   (0, 1, 0),
            "yellow": (1, 1, 0), "blue": (0, 0, 1), "magenta": (1, 0, 1),
            "cyan": (0, 1, 1), "white": (1, 1, 1)}
