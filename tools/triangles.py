9df06cb4c7571b06fa7d1127a398cc90d711dcb3
David Boddie <david@boddie.org.uk>
2015-10-13 01:01:24 +0200

    Updated the triangle tiling program to use fixed data rather than random colours.

--- tools/triangles.py
+++ tools/triangles.py
@@ -38,44 +38,59 @@ rw = rh * 2
 scanlines = []
 row = 0
 
-colour0 = colours[random.randrange(0, 7)]
-colour1 = colour0
+row_colours = [(Black, Red), (Red, Yellow), (Red, Blue), (Magenta, Green),
+               (Red, Green), (Black, Green), (Blue, Green), (Red, Green),
+               (Black, Green), (Red, Yellow), (Yellow, Blue), (Blue, Green),
+               (Green, Magenta), (Green, Cyan), (White, Green), (White, Black)]
+
+tiles = [(0,1, 1,1, 1,1, 0,0, 0,1, 0,0, 0,1, 0,0, 0,1, 0,0, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,0, 1,2, 2,1, 0,0, 0,0, 0,0, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (1,1, 0,1, 1,1, 1,1, 0,1, 1,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,2, 0,1, 0,1, 0,1, 0,1, 1,0, 0,1, 0,1),
+         (0,1, 1,0, 0,1, 1,0, 2,2, 2,1, 1,2, 2,0, 1,1, 1,0, 0,0, 1,1, 1,0, 0,1, 0,2, 0,1, 1,0, 0,1, 1,0, 0,1),
+         (1,1, 2,1, 1,2, 1,1, 0,1, 1,0, 0,1, 1,0, 1,1, 0,1, 1,0, 1,1, 1,0, 0,1, 0,1, 1,0, 0,1, 2,1, 0,1, 1,0),
+         (1,1, 0,2, 2,0, 1,1, 0,1, 1,0, 0,1, 1,0, 1,1, 0,1, 1,0, 1,1, 1,0, 0,1, 0,1, 1,0, 1,2, 1,0, 0,1, 1,0),
+         (1,0, 0,2, 2,0, 0,1, 0,0, 0,1, 1,0, 0,0, 1,1, 1,0, 0,0, 1,1, 1,0, 0,1, 0,1, 0,1, 1,0, 0,1, 1,0, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 1,0, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1),
+         (0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1)]
+
+orientations = [
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////",
+    r"/\/\//\\///\/////\//",
+    r"/\/\//\\\\///////\\/",
+    r"/\/\//\\/\\/\////\\\ ",
+    r"/\/\\\//\//\//\\\///",
+    r"//\\\\////\\\//\\///",
+    r"////////////////\///",
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////",
+    r"////////////////////"
+    ]
 
 while y < 256:
 
-    # Choose a new second colour for the row.
-    while True:
-        new_colour = colours[random.randrange(0, 7)]
-        if new_colour != colour1 and new_colour != colour0:
-            colour0 = colour1
-            colour1 = new_colour
-            break
-    
+    colour0, colour1 = row_colours[row]
     rgb_values = [(colour0, colour0), (colour1, colour1), (colour0, colour1)]
+    tile_colours = tiles[row]
+
     ry = 0
-    possible = [0, 1, 2]
-    tile_colours = []
-    i = 0
-    while i < 2 * (640/rw):
-        if not tile_colours:
-            tile_colours.append(random.choice(rgb_values))
-        else:
-            while True:
-                colour = random.choice(rgb_values)
-                if colour != tile_colours[-1]:
-                    tile_colours.append(colour)
-                    break
-        i += 1
     
     while ry < rh:
     
         scanline = ""
         x = 0
-        # Change the span length for each scanline.
-        if row % 4 == 0 or row % 4 == 3:
-            tw = ry * (rw/rh) + 1
-        else:
-            tw = rw - (ry * (rw/rh)) - 1
         
         # Maintain an index into the tile colour list.
         c = 0
@@ -83,12 +98,22 @@ while y < 256:
         while x < 640:
         
             # Find the colour of this tile.
-            colour = tile_colours[c]
+            colour = rgb_values[tile_colours[c]]
+            
+            # Find the length of the span - this applies to two adjacent tiles.
+            orientation = orientations[row][c/2]
+            tw = ry * (rw/rh) + 1
+            
+            if orientation == "\\":
+                if c % 2 == 1:
+                    tw = rw - tw
+            elif c % 2 == 0:
+                tw = rw - tw
             
             # Add the pixel values for the colour, alternating between the two
             # supplied values and starting with the second value on alternate
             # scanlines.
-            p = ry % 2
+            p = (ry + x) % 2
             i = 0
             while i < min(640 - x, tw):
                 scanline += colour[p]
@@ -96,11 +121,6 @@ while y < 256:
                 p = 1 - p
             
             x += tw
-            
-            # Mirror the triangles every two triangles.
-            if c % 2 == 0:
-                tw = rw - tw
-            
             c += 1
         
         scanlines.append(scanline)
